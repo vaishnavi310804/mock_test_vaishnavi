b216317924df0ead7a1715b8db1d9e0ab2c9932c
vaishnavi310804 <vaishnavikumari3108@gmail.com>
2025-11-10 13:47:29 +0530

    Done

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,57 +1,13 @@
-import React from "react";
-import {
-  View,
-  Text,
-  StyleSheet,
-  TextInput,
-  ScrollView,
-  TouchableOpacity,
-} from "react-native";
-import { Ionicons } from "@expo/vector-icons";
-import Navbar from "@/components/Navbar";
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
 import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
-import Card from "@/components/Card";
+import { Ionicons } from "@expo/vector-icons";
 
-const Secondscreen = () => {
+
+const Card = () => {
   return (
-    <ScrollView
-      style={styles.container}
-      contentContainerStyle={styles.scrollContent}
-      showsVerticalScrollIndicator={false}
-    >
-        <Navbar />
-      <View style={styles.header}>
-        <Text style={styles.title}>Library</Text>
-        <Text style={styles.subtitle}>Doctor’s approved audio episodes</Text>
-      </View>
-
-      <View style={styles.searchBar}>
-        <Ionicons name="search-outline" size={20} color="#999" />
-        <TextInput
-          placeholder="Search for symptom or condition..."
-          placeholderTextColor="#999"
-          style={styles.searchInput}
-        />
-      </View>
-
-      <View style={styles.tagsContainer}>
-        <TouchableOpacity style={styles.tag}>
-          <Ionicons name="grid-outline" size={16} color="#0ea5e9" />
-          <Text style={styles.tagText}>Categories</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity style={styles.tag}>
-          <Ionicons name="pulse-outline" size={16} color="#0ea5e9" />
-          <Text style={styles.tagText}>Symptoms</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity style={styles.tag}>
-          <Ionicons name="time-outline" size={16} color="#0ea5e9" />
-          <Text style={styles.tagText}>Duration</Text>
-        </TouchableOpacity>
-      </View>
-
-      {/* <View style={styles.card}>
+    <View>
+         <View style={styles.card}>
         <View style={[styles.iconBox, { backgroundColor: "#E8F9F0" }]}>
           <FontAwesome5 name="lungs" size={28} color="#22B07D" />
         </View>
@@ -100,55 +56,15 @@ const Secondscreen = () => {
           <Text style={styles.cardSub}>What an MRI can tell you</Text>
           <Text style={styles.moreText}>+ 10 more</Text>
         </View>
-      </View> */}
+      </View>
+    </View>
+  )
+}
 
-      <Card/>
-    </ScrollView>
-  );
-};
-
-export default Secondscreen;
+export default Card
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#e4f0fc",
-  },
-  scrollContent: {
-    padding: 20,
-    paddingBottom: 40,
-  },
-  header: {
-    marginTop: 10,
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: "800",
-    color: "#0f172a",
-  },
-  subtitle: {
-    fontSize: 14,
-    color: "#475569",
-    marginTop: 4,
-  },
-  searchBar: {
-    flexDirection: "row",
-    backgroundColor: "#fff",
-    borderRadius: 25,
-    paddingHorizontal: 14,
-    paddingVertical: 10,
-    alignItems: "center",
-    marginTop: 16,
-    shadowColor: "#000",
-    shadowOpacity: 0.05,
-    shadowRadius: 5,
-    elevation: 2,
-  },
-  searchInput: {
-    marginLeft: 8,
-    flex: 1,
-    color: "#333",
-  },
+
   tagsContainer: {
     flexDirection: "row",
     justifyContent: "space-between",
